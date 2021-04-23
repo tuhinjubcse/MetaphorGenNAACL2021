7ccb68c83b5786c4477a4e699ec7e203b03d77e3
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2021-04-23 09:28:52 -0400

    Update preprocess2.sh

--- fairseq/preprocess2.sh
+++ fairseq/preprocess2.sh
@@ -1,9 +1,9 @@
 fairseq-preprocess \
   --source-lang "source" \
   --target-lang "target" \
-  --trainpref "freebase/train.bpe" \
-  --validpref "freebase/val.bpe" \
-  --destdir "freebase/" \
+  --trainpref "metaphor/train.bpe" \
+  --validpref "metaphor/val.bpe" \
+  --destdir "metaphor/" \
   --workers 60 \
   --srcdict dict.txt \
   --tgtdict dict.txt;
